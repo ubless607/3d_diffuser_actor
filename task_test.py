d2039a88e881b40b6a4ad3d8a53988fc2d89b48a
ubless607 <tjdwo7friend@naver.com>
2024-09-06 14:34:49 +0900

    Update task_test.py

--- task_test.py
+++ task_test.py
@@ -9,11 +9,17 @@ from rlbench.backend.scene import Scene
 from rlbench.backend.utils import task_file_to_task_class
 from rlbench.backend.task import TASKS_PATH
 from rlbench.backend.robot import Robot
+from rlbench.action_modes.arm_action_modes import EndEffectorPoseViaPlanning
+from rlbench.action_modes.gripper_action_modes import Discrete
+from rlbench.environment import TaskEnvironment
+from rlbench.action_modes.action_mode import MoveArmThenGripper
 from pyrep.const import RenderMode
+from rlbench.const import colors
 import numpy as np
 import os
 import argparse
-import pickle
+import json
+from PIL import Image
 
 DEMO_ATTEMPTS = 5
 MAX_VARIATIONS = 100
@@ -23,15 +29,18 @@ class TaskValidationError(Exception):
     pass
 
 
-def task_smoke(task: Task, scene: Scene, variation=-1, demos=1, success=0.50,
+def task_smoke(task: Task, scene: Scene, variation=-1, demos=200, success=0.50,
                max_variations=3, test_demos=True):
     print('Running task validator on task: %s' % task.get_name())
 
     # Loading
     scene.load(task)
+    global count
+    count = 0
 
     # Number of variations
     variation_count = task.variation_count()
+    total_runs = int(demos // variation_count) if variation == -1 else demos
     if variation_count < 0:
         raise TaskValidationError(
             "The method 'variation_count' should return a number > 0.")
@@ -60,7 +69,6 @@ def task_smoke(task: Task, scene: Scene, variation=-1, demos=1, success=0.50,
 
         attempt_result = False
         failed_demos = 0
-        
         for j in range(DEMO_ATTEMPTS):
             failed_demos = run_demos(i)
             attempt_result = (failed_demos / float(demos) <= 1. - success)
@@ -78,15 +86,33 @@ def task_smoke(task: Task, scene: Scene, variation=-1, demos=1, success=0.50,
             print('Variation %d of task %s is good!' % (i, task.get_name()))
             if test_demos:
                 print('%d of %d demos were successful.' % (
-                    demos - failed_demos, demos))
+                    total_runs - failed_demos, total_runs))
 
     def run_demos(variation_num):
+        global count
         fails = 0
-        for dr in range(demos):
+        for idx in range(total_runs):
             try:
                 scene.reset()
                 desc = scene.init_episode(variation_num, max_attempts=10)
+
                 origin = task.boundary_root().get_position(), task.boundary_root().get_orientation()
+                if idx == 0:
+                    A_d, A_s= task.lightA.get_diffuse(), task.lightA.get_specular()
+                    B_d, B_s= task.lightB.get_diffuse(), task.lightB.get_specular()
+                    D_d, D_s= task.lightD.get_diffuse(), task.lightD.get_specular()
+
+                light_cond = np.random.random()
+                if light_cond > 0.8:
+                    color_idx = np.random.randint(0, len(colors))
+                    color_rgb = colors[color_idx][1]
+                    light = np.random.choice([task.lightA, task.lightB, task.lightD], 1)[0]
+                    light.set_diffuse(color_rgb)
+                    light.set_specular(color_rgb)
+                else:
+                    task.lightA.set_diffuse(A_d); task.lightA.set_specular(A_s)
+                    task.lightB.set_diffuse(B_d); task.lightB.set_specular(B_s)
+                    task.lightD.set_diffuse(D_d); task.lightD.set_specular(D_s)
                 if not isinstance(desc, list) or len(desc) <= 0:
                     raise TaskValidationError(
                         "The method 'init_variation' should return a list of "
@@ -94,39 +120,64 @@ def task_smoke(task: Task, scene: Scene, variation=-1, demos=1, success=0.50,
                 if test_demos:
                     # inference with gt position
                     demo = scene.get_demo(record=True)
-                    state_ = {'variation_num': variation_num,
-                                'left_gt_coord' : task.left,
-                                'left_gt_orientation': task.left_orient,
-                                'right_gt_coord': task.right,
-                                'right_gt_orientation': task.right_orient}
+                    os.makedirs(f'./eval_logs/{task_name}/gt', exist_ok=True)
+                    img = Image.fromarray(demo._observations[-1].wrist_rgb)
+                    img.save(f'./eval_logs/{task_name}/gt/{count:03d}.png')
+                    state_ = {'variation_num': count,
+                                'left_gt_coord' : task.left.tolist(),
+                                'left_gt_orientation': task.left_orient.tolist(),
+                                'right_gt_coord': task.right.tolist(),
+                                'right_gt_orientation': task.right_orient.tolist(),}
                     json_dump.append(state_)
             except DemoError as e:
                 fails += 1
                 print(e)
+                continue
             except Exception as e:
                 # TODO: check that we don't fall through all of these cases
                 fails += 1
                 print(e)
-            
+
             scene.reset()
             desc = scene.init_episode(variation_num, max_attempts=10, randomly_place=False)
+            
+            if light_cond > 0.8:
+                light.set_diffuse(color_rgb)
+                light.set_specular(color_rgb)
+            else:
+                task.lightA.set_diffuse(A_d); task.lightA.set_specular(A_s)
+                task.lightB.set_diffuse(B_d); task.lightB.set_specular(B_s)
+                task.lightD.set_diffuse(D_d); task.lightD.set_specular(D_s)      
             task.boundary_root().set_position(origin[0])
             task.boundary_root().set_orientation(origin[1])
             task.augment()
-            demo2 = scene.get_demo(record=True, randomly_place=False)
-            state_ = json_dump[-1]
-            state_['left_coord'] = task.left
-            state_['left_orientation'] = task.left_orient
-            state_['right_coord'] = task.right
-            state_['right_orientation'] = task.right_orient
-            json_dump.append(state_)
+            print(task.rot_aug, task.pos_aug)
+            
+            try:
+                demo2 = scene.get_demo(record=True, randomly_place=False)
+                os.makedirs(f'./eval_logs/{task_name}/aug', exist_ok=True)
+                img = Image.fromarray(demo2._observations[-1].wrist_rgb)
+                img.save(f'./eval_logs/{task_name}/aug/{count:03d}_aug1.png')
+
+                state_ = json_dump[-1]
+                json_dump.pop()
+                state_['left_coord'] = task.left.tolist()
+                state_['left_orientation'] = task.left_orient.tolist()
+                state_['right_coord'] = task.right.tolist()
+                state_['right_orientation'] = task.right_orient.tolist()
+                state_['rot_aug'] = task.rot_aug
+                state_['pos_aug'] = task.pos_aug
+                json_dump.append(state_)
+            except DemoError as e:
+                fails += 1
+                print(e)
+                continue
+            finally:
+                count += 1
         return fails
 
-    variations_to_test = [variation]
-    if variation < 0:
-        variations_to_test = list(range(
-            np.minimum(variation_count, max_variations)))
-
+    variations_to_test = range(variation_count) if variation == -1 else [variation]
+    print(variations_to_test)
     # Task set-up
     scene.init_task()
     [variation_smoke(i) for i in variations_to_test]
@@ -135,6 +186,8 @@ def task_smoke(task: Task, scene: Scene, variation=-1, demos=1, success=0.50,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("task", help="The task file to test.")
+    parser.add_argument("--headless", action='store_true')
+    parser.add_argument("--collision_checking", action='store_true')
     args = parser.parse_args()
 
     python_file = os.path.join(TASKS_PATH, args.task)
@@ -143,21 +196,17 @@ if __name__ == '__main__':
 
     task_class = task_file_to_task_class(args.task)
     sim = PyRep()
-    ttt_file = os.path.join(
-        '/home/commonsense/data/cvpr/3d_diffuser_actor/RLBench/rlbench', TTT_FILE)
-    
-    json_dump = []
-    
-    sim.launch(ttt_file, headless=False, responsive_ui=True)
+    ttt_file = os.path.join('/home/commonsense/data/cvpr/3d_diffuser_actor/RLBench/rlbench', TTT_FILE)
+
+    sim.launch(ttt_file, headless=args.headless, responsive_ui=True)
     sim.step_ui()
     sim.set_simulation_timestep(0.005)
     sim.step_ui()
     sim.start()
-
     robot = Robot(Panda(), PandaGripper())
-
     active_task = task_class(sim, robot)
-    
+    task_name = active_task.get_name()
+
     # camera
     obs = ObservationConfig()
     obs.set_all(False)
@@ -166,11 +215,30 @@ if __name__ == '__main__':
     obs.wrist_camera = cam_config
     scene = Scene(sim, robot, obs)
 
+    os.makedirs('./eval_logs', exist_ok=True)
+    os.makedirs(f'./eval_logs/{task_name}', exist_ok=True)
+    json_dump = []
     try:
-        task_smoke(active_task, scene, variation=1)
+        task_smoke(active_task, scene, variation=-1)
     except TaskValidationError as e:
         sim.shutdown()
         raise e
     sim.shutdown()
-    print(json_dump)
+    
+    # post-process
+    tmp_lst = []
+    for i, case in enumerate(json_dump):
+        try:
+            case['left_coord']
+        except KeyError:
+            tmp_lst.append(i)
+    
+    json_dump = [json_dump[i] for i in range(len(json_dump)) if i not in tmp_lst]
+    for directory in ['gt', 'aug']:
+        for filename in os.listdir(f'./eval_logs/{task_name}/{directory}'):
+            if any(str(index) in filename for index in tmp_lst):
+                os.remove(f'./eval_logs/{task_name}/{directory}/{filename}')
+
+    with open(f'./eval_logs/{task_name}/log.json', 'w') as f:
+        json.dump(json_dump, f, indent=4)
     print('Validation successful!')
